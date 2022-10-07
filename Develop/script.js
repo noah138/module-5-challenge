71786db81159ccbd172c93bea52a4d421facafce
noah138 <noahschwartz@live.com>
2022-10-06 23:37:08 -0700

    added function to change the row colors depending on the time

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,45 @@
+$(document).ready(function () {
+    colorChanger();
+    displayDate();
+})
+
 // Displaying the current date
 var dateDisplay = $("#currentDay");
 
-displayDate();
-
 function displayDate() {
     var now = moment().format("dddd, MMMM Do");
-    dateDisplay.text(now)
+    dateDisplay.text(now);
 }
 
-// How to get colors to change in relation to time:
-// 1. make variable that saves current hour of the day
-// 2. make a loop that goes through each row and checks hour variable against it own
-// 3. if less than, equal to, or greater than add the correct css class
 
-$(document).ready(function () {
+function colorChanger() {
+    // retrieve current number of hours
+    var currentHour = moment().hour()
+    // loop through each row
+    $(".time-block").each(function () {
+        var rowHour = parseInt($(this).attr("id"));
+        console.log(rowHour)
+        // to compare rowHour with currentHour and change color accordingly
+        if (rowHour < currentHour) {
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+        else if (rowHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    })
+}
+// Not sure how to incorporate AM and PM elements into the color changes
+
+
     // save button function
     $(".saveBtn").on("click", function() {
         // retrieve the values of the bordering boxes then save text
@@ -21,4 +47,3 @@ $(document).ready(function () {
         var time = $(this).siblings(".hour").val();
         localStorage.setItem(time, description);
     })
-})
