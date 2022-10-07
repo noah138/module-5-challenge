5d26e0c0b882d17fa6f3b85ac988d86ecc4dce5b
noah138 <noahschwartz@live.com>
2022-10-06 22:52:24 -0700

    Did the JS for the save button and pseudocoded the JS for the color changing

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Displaying the current date
-var dateDisplay = $('#currentDay');
+var dateDisplay = $("#currentDay");
 
 displayDate();
 
@@ -8,3 +8,17 @@ function displayDate() {
     dateDisplay.text(now)
 }
 
+// How to get colors to change in relation to time:
+// 1. make variable that saves current hour of the day
+// 2. make a loop that goes through each row and checks hour variable against it own
+// 3. if less than, equal to, or greater than add the correct css class
+
+$(document).ready(function () {
+    // save button function
+    $(".saveBtn").on("click", function() {
+        // retrieve the values of the bordering boxes then save text
+        var description = $(this).siblings(".description").val();
+        var time = $(this).siblings(".hour").val();
+        localStorage.setItem(time, description);
+    })
+})
